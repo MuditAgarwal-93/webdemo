1f103a6b809ade9ff876bf643b9e967d0083de9b
Mudit Agarwal <muditagarwal9391@gmail.com>
2025-07-31 18:24:39 +0530

    website updated till testing secion

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const menu = document.querySelector(".menu");
+const nav_link = document.querySelector(".navLinks");
+
+menu.addEventListener('click',() => {
+nav_link.classList.toggle('active')
+}
+)
+;
 
